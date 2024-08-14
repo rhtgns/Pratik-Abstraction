f75cee685e86d6f715f61a6f91c852ca1f859e5d
rohat güneş <rohatgns44@gmail.com>
2024-08-14 23:57:53 +1000

    Add project files.

---
+++ Abstraction/Class1.cs
@@ -0,0 +1,48 @@
+﻿using CoWorkers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    internal class Workers
+    {
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Yazılım Geliştirici nesnesi oluşturma ve metodu çağırma
+                SoftwareDeveloper yazilimci = new SoftwareDeveloper
+                {
+                    Name = "Ahmet",
+                    SurName= "Yılmaz",
+                    Department ="yazılımcı"
+                    
+                    
+                };
+                yazilimci.mission();
+
+                // Proje Yöneticisi nesnesi oluşturma ve metodu çağırma
+                ProjectManager yonetici = new ProjectManager
+                {
+                    Name = "Hasan",
+                    SurName = "Çıldırmış",
+                    Department = "Proje Yönetimi"
+                };
+                yonetici.mission();
+
+                // Satış Temsilcisi nesnesi oluşturma ve metodu çağırma
+                Seller seller = new Seller
+                {
+                    Name = "Zeynep",
+                    SurName = "Kaya",
+                    Department = "Satış"
+                };
+                seller.mission();
+            }
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace CoWorkers
+{
+    // burda soyut yani abstract sınıfı oluituralım
+
+    public abstract class Workers
+    {
+
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Department { get; set; }
+
+
+
+        // ardından simdi abstract metofumu cagıralım
+
+        public abstract void mission();
+
+
+
+    }
+
+    // burda yazılım geliştirici sınfını yapalım
+
+    public class SoftwareDeveloper : Workers
+    {
+        public override void mission()
+        {
+            Console.WriteLine($"{Name} {SurName} sirketin yazılım geliştiricisi , Yazılım geliştiricisi olarak çalışıyorum ");
+        }
+
+
+    }
+
+    public class  Seller : Workers
+    {
+        public override void mission()
+        {
+            Console.WriteLine($"{Name} {SurName} şirketin satıs temsilcisi, satış temsilcisi olarak çalışıyorum");
+        }
+    }
+    public  class ProjectManager  : Workers
+    {
+        public override void mission()
+        {
+            Console.WriteLine($" {Name} {SurName} sirketin proje yöneticisi , projede yonetici olarak çalışıyorum");
+        }
+    }
+}
